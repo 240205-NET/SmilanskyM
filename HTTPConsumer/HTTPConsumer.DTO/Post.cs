1a9536b1ba3bf8db70681b402eec505dc62b7b10
Gumshoe <msmilansky@gmail.com>
2024-02-21 06:35:07 -0800

    Adding material from W3D2

---
+++ HTTPConsumer/HTTPConsumer.DTO/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace HTTPConsumer.DTO
+{
+    public class Post
+    {
+        // fields
+        public int id { get; set; }
+        public int userId { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        // constructors
+
+        // methods
+    }
+}
